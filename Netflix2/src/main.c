62e90d72d8f962f2664ca91d635fc7c0b3fc23be
erikha1501 <maskdroid01@gmail.com>
2020-12-31 13:01:55 +0700

    Fix make file

--- Netflix2/src/main.c
+++ Netflix2/src/main.c
@@ -14,7 +14,6 @@ struct CastEntry;
 struct GenreEntry;
 struct Database;
 
-
 typedef struct _MovieEntry
 {
     int show_id;
@@ -22,33 +21,33 @@ typedef struct _MovieEntry
     int release_year;
     int duration;
 
-    const char* description;
+    const char *description;
 
     int director_count;
-    struct DirectorEntry** directors;
+    struct DirectorEntry **directors;
 
     int cast_count;
-    struct CastEntry** casts;
+    struct CastEntry **casts;
 
     int genre_count;
-    struct GenreEntry** genres;
+    struct GenreEntry **genres;
 } MovieEntry;
 
 typedef struct _DirectorEntry
 {
-    const char* name;
+    const char *name;
     Dllist movies;
 } DirectorEntry;
 
 typedef struct _CastEntry
 {
-    const char* name;
+    const char *name;
     Dllist movies;
 } CastEntry;
 
 typedef struct _GenreEntry
 {
-    const char* name;
+    const char *name;
     Dllist movies;
 } GenreEntry;
 
@@ -56,19 +55,19 @@ typedef struct _Database
 {
     int movies_capacity;
     int movies_count;
-    MovieEntry* movies;
+    MovieEntry *movies;
 
     int directors_capacity;
     int directors_count;
-    DirectorEntry* directors;
+    DirectorEntry *directors;
 
     int casts_capacity;
     int casts_count;
-    CastEntry* casts;
+    CastEntry *casts;
 
     int genres_capacity;
     int genres_count;
-    GenreEntry* genres;
+    GenreEntry *genres;
 
     // Key -> name, value -> MovieEntry*
     JRB movies_name_lookup;
@@ -80,62 +79,62 @@ typedef struct _Database
     JRB genres_name_lookup;
 } Database;
 
-void database_init(Database* database)
+void database_init(Database *database)
 {
     // Movies
     database->movies_capacity = 6300;
     database->movies_count = 0;
-    database->movies = (MovieEntry*)malloc(sizeof(MovieEntry) * database->movies_capacity);
+    database->movies = (MovieEntry *)malloc(sizeof(MovieEntry) * database->movies_capacity);
 
     // Directors
     database->directors_capacity = 200;
     database->directors_count = 0;
-    database->directors = (DirectorEntry*)malloc(sizeof(DirectorEntry) * database->directors_capacity);
+    database->directors = (DirectorEntry *)malloc(sizeof(DirectorEntry) * database->directors_capacity);
 
     // Casts
     database->casts_capacity = 400;
     database->casts_count = 0;
-    database->casts = (CastEntry*)malloc(sizeof(CastEntry) * database->casts_capacity);
+    database->casts = (CastEntry *)malloc(sizeof(CastEntry) * database->casts_capacity);
 
     // Genres
     database->genres_capacity = 35;
     database->genres_count = 0;
-    database->genres = (GenreEntry*)malloc(sizeof(GenreEntry) * database->genres_capacity);
+    database->genres = (GenreEntry *)malloc(sizeof(GenreEntry) * database->genres_capacity);
 
     // Populate genres list
-    database->genres[0] = (GenreEntry){ .name = "SciFi", .movies = new_dllist() };
-    database->genres[1] = (GenreEntry){ .name = "Kids", .movies = new_dllist() };
-    database->genres[2] = (GenreEntry){ .name = "Comedies", .movies = new_dllist() };
-    database->genres[3] = (GenreEntry){ .name = "Standup", .movies = new_dllist() };
-    database->genres[4] = (GenreEntry){ .name = "Fantasy", .movies = new_dllist() };
-    database->genres[5] = (GenreEntry){ .name = "Crime", .movies = new_dllist() };
-    database->genres[6] = (GenreEntry){ .name = "Spanish", .movies = new_dllist() };
-    database->genres[7] = (GenreEntry){ .name = "International", .movies = new_dllist() };
-    database->genres[8] = (GenreEntry){ .name = "Thrillers", .movies = new_dllist() };
-    database->genres[9] = (GenreEntry){ .name = "Comedy", .movies = new_dllist() };
-    database->genres[10] = (GenreEntry){ .name = "Docuseries", .movies = new_dllist() };
-    database->genres[11] = (GenreEntry){ .name = "Science", .movies = new_dllist() };
-    database->genres[12] = (GenreEntry){ .name = "Nature", .movies = new_dllist() };
-    database->genres[13] = (GenreEntry){ .name = "Action", .movies = new_dllist() };
-    database->genres[14] = (GenreEntry){ .name = "Adventure", .movies = new_dllist() };
-    database->genres[15] = (GenreEntry){ .name = "Dramas", .movies = new_dllist() };
-    database->genres[16] = (GenreEntry){ .name = "Cult", .movies = new_dllist() };
-    database->genres[17] = (GenreEntry){ .name = "Indie", .movies = new_dllist() };
-    database->genres[18] = (GenreEntry){ .name = "Romantic", .movies = new_dllist() };
-    database->genres[19] = (GenreEntry){ .name = "Documentaries", .movies = new_dllist() };
-    database->genres[20] = (GenreEntry){ .name = "Horror", .movies = new_dllist() };
-    database->genres[21] = (GenreEntry){ .name = "Mysteries", .movies = new_dllist() };
-    database->genres[22] = (GenreEntry){ .name = "British", .movies = new_dllist() };
-    database->genres[23] = (GenreEntry){ .name = "Movies", .movies = new_dllist() };
-    database->genres[24] = (GenreEntry){ .name = "Music", .movies = new_dllist() };
-    database->genres[25] = (GenreEntry){ .name = "Reality", .movies = new_dllist() };
-    database->genres[26] = (GenreEntry){ .name = "Anime", .movies = new_dllist() };
-    database->genres[27] = (GenreEntry){ .name = "Teen", .movies = new_dllist() };
-    database->genres[28] = (GenreEntry){ .name = "Sports", .movies = new_dllist() };
-    database->genres[29] = (GenreEntry){ .name = "Spirituality", .movies = new_dllist() };
-    database->genres[30] = (GenreEntry){ .name = "Korean", .movies = new_dllist() };
-    database->genres[31] = (GenreEntry){ .name = "LGBTQ", .movies = new_dllist() };
-    database->genres[32] = (GenreEntry){ .name = "Classic", .movies = new_dllist() };
+    database->genres[0] = (GenreEntry){.name = "SciFi", .movies = new_dllist()};
+    database->genres[1] = (GenreEntry){.name = "Kids", .movies = new_dllist()};
+    database->genres[2] = (GenreEntry){.name = "Comedies", .movies = new_dllist()};
+    database->genres[3] = (GenreEntry){.name = "Standup", .movies = new_dllist()};
+    database->genres[4] = (GenreEntry){.name = "Fantasy", .movies = new_dllist()};
+    database->genres[5] = (GenreEntry){.name = "Crime", .movies = new_dllist()};
+    database->genres[6] = (GenreEntry){.name = "Spanish", .movies = new_dllist()};
+    database->genres[7] = (GenreEntry){.name = "International", .movies = new_dllist()};
+    database->genres[8] = (GenreEntry){.name = "Thrillers", .movies = new_dllist()};
+    database->genres[9] = (GenreEntry){.name = "Comedy", .movies = new_dllist()};
+    database->genres[10] = (GenreEntry){.name = "Docuseries", .movies = new_dllist()};
+    database->genres[11] = (GenreEntry){.name = "Science", .movies = new_dllist()};
+    database->genres[12] = (GenreEntry){.name = "Nature", .movies = new_dllist()};
+    database->genres[13] = (GenreEntry){.name = "Action", .movies = new_dllist()};
+    database->genres[14] = (GenreEntry){.name = "Adventure", .movies = new_dllist()};
+    database->genres[15] = (GenreEntry){.name = "Dramas", .movies = new_dllist()};
+    database->genres[16] = (GenreEntry){.name = "Cult", .movies = new_dllist()};
+    database->genres[17] = (GenreEntry){.name = "Indie", .movies = new_dllist()};
+    database->genres[18] = (GenreEntry){.name = "Romantic", .movies = new_dllist()};
+    database->genres[19] = (GenreEntry){.name = "Documentaries", .movies = new_dllist()};
+    database->genres[20] = (GenreEntry){.name = "Horror", .movies = new_dllist()};
+    database->genres[21] = (GenreEntry){.name = "Mysteries", .movies = new_dllist()};
+    database->genres[22] = (GenreEntry){.name = "British", .movies = new_dllist()};
+    database->genres[23] = (GenreEntry){.name = "Movies", .movies = new_dllist()};
+    database->genres[24] = (GenreEntry){.name = "Music", .movies = new_dllist()};
+    database->genres[25] = (GenreEntry){.name = "Reality", .movies = new_dllist()};
+    database->genres[26] = (GenreEntry){.name = "Anime", .movies = new_dllist()};
+    database->genres[27] = (GenreEntry){.name = "Teen", .movies = new_dllist()};
+    database->genres[28] = (GenreEntry){.name = "Sports", .movies = new_dllist()};
+    database->genres[29] = (GenreEntry){.name = "Spirituality", .movies = new_dllist()};
+    database->genres[30] = (GenreEntry){.name = "Korean", .movies = new_dllist()};
+    database->genres[31] = (GenreEntry){.name = "LGBTQ", .movies = new_dllist()};
+    database->genres[32] = (GenreEntry){.name = "Classic", .movies = new_dllist()};
     database->genres_count = 33;
 
     // Initialize lookup trees
@@ -151,22 +150,28 @@ void database_init(Database* database)
     }
 }
 
-
-const char* duplicate_string(const c_string_view str)
+const char *duplicate_string(const c_string_view str)
 {
-    const char* buf = (const char*)malloc(sizeof(char) * str.size);
+    const char *buf = (const char *)malloc(sizeof(char) * str.size);
     strncpy(buf, str.str, str.size);
 
     return buf;
 }
 
-DirectorEntry* database_insert_director(Database* database, const char* name);
-CastEntry* database_insert_cast(Database* database, const char* name);
-MovieEntry* database_insert_movie(Database* database, MovieInfo* movieInfo);
+DirectorEntry *database_insert_director(Database *database, const char *name);
+CastEntry *database_insert_cast(Database *database, const char *name);
+MovieEntry *database_insert_movie(Database *database, const MovieInfo *movieInfo);
 
-MovieEntry* database_insert_movie(Database* database, const MovieInfo* movieInfo)
+DirectorEntry *database_insert_director(Database *database, const char *name)
 {
-    MovieEntry* movie = &database->movies[database->movies_count++];
+}
+CastEntry *database_insert_cast(Database *database, const char *name)
+{
+}
+
+MovieEntry *database_insert_movie(Database *database, const MovieInfo *movieInfo)
+{
+    MovieEntry *movie = &database->movies[database->movies_count++];
     movie->show_id = movieInfo->show_id;
     movie->description = duplicate_string(movieInfo->description);
     movie->duration = movieInfo->duration;
@@ -175,7 +180,7 @@ MovieEntry* database_insert_movie(Database* database, const MovieInfo* movieInfo
 
     // Add genres
     movie->genre_count = movieInfo->genre_count;
-    movie->genres = (GenreEntry**)malloc(sizeof(GenreEntry**) * movie->genre_count);
+    movie->genres = (GenreEntry **)malloc(sizeof(GenreEntry **) * movie->genre_count);
     for (int i = 0; i < movieInfo->genre_count; i++)
     {
         // Genre value is the index into database-genres
@@ -184,28 +189,24 @@ MovieEntry* database_insert_movie(Database* database, const MovieInfo* movieInfo
 
     // Insert directors
     movie->director_count = movieInfo->director_count;
-    movie->directors = (DirectorEntry**)malloc(sizeof(DirectorEntry*) * movie->director_count);
+    movie->directors = (DirectorEntry **)malloc(sizeof(DirectorEntry *) * movie->director_count);
     for (int i = 0; i < movieInfo->directors->size; i++)
     {
-        DirectorEntry* insertedDirector = database_insert_director(database, duplicate_string(movieInfo->directors[i]));
+        DirectorEntry *insertedDirector = database_insert_director(database, duplicate_string(movieInfo->directors[i]));
         movie->directors[i] = insertedDirector;
     }
 
     // Insert casts
     movie->cast_count = movieInfo->cast_count;
-    movie->casts = (CastEntry**)malloc(sizeof(CastEntry*) * movie->cast_count);
+    movie->casts = (CastEntry **)malloc(sizeof(CastEntry *) * movie->cast_count);
     for (int i = 0; i < movieInfo->casts->size; i++)
     {
-        CastEntry* insertedCast = database_insert_cast(database, duplicate_string(movieInfo->casts[i]));
+        CastEntry *insertedCast = database_insert_cast(database, duplicate_string(movieInfo->casts[i]));
         movie->casts[i] = insertedCast;
     }
 }
 
-
-
-
-
-void callback(const MovieInfo* movieInfo)
+void callback(const MovieInfo *movieInfo)
 {
     char buf[50];
 
@@ -217,13 +218,12 @@ void callback(const MovieInfo* movieInfo)
     //    buf[movieInfo-ncasts[i].size + 1] = '\0';
     //    printf("%s, ", buf);
     //}
-    printf("%ld\n", movieInfo->genreBitFields);
 
     printf("\n");
 }
 
 int main()
 {
-    const char* path = "./data/netflix_titles2.csv";
+    const char *path = "./data/netflix_titles2.csv";
     read_csv(path, callback);
 }
